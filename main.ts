1568e4e48b4aa384b3a70121c57c8919c343ed38
xubone <69479213+xubone@users.noreply.github.com>
2020-09-13 11:11:41 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("sebas")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("casper")
+})
+basic.forever(function () {
+	
+})
